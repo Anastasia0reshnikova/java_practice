97841320539c6d4dde0a242e931a4609616315e8
Anastasia0reshnikova <piter_ao12@mail.ru>
2017-11-05 17:35:41 +0300

    Задание 7. Добавлена возможность запускать тесты в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,21 +14,33 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-    WebDriver wd;
+    private WebDriver wd;
 
     private NavigationHelper navigationHelper;
-    private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver");
-        wd = new FirefoxDriver();
+        if(browser.equals(BrowserType.FIREFOX)) {
+            System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver");
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+            wd = new ChromeDriver();
+        } else  if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost:8085/addressbook/index.php");
-        groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
+        groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
         sessionHelper.login("admin", "secret");
     }
