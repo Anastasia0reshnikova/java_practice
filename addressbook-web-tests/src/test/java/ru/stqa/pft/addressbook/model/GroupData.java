9f2ac5149681fed602210df1f4ebec28fb4729b7
Anastasia0reshnikova <piter_ao12@mail.ru>
2017-12-04 22:41:54 +0300

    1.ТД для групп генерируются в файл.xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,7 +1,12 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
+@XStreamAlias("group")
 public class GroupData {
 
+    @XStreamOmitField //пропустить поле, не сохранять в xml
     private int id= Integer.MAX_VALUE;
     private String name;
     private String header;
